e4b4e344c14a136806beb55e0574639fc8a34607
DieKon <medvedev-1985@yandex.ru>
2022-09-25 17:09:15 +0300

    new logo and structure

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" className="App-logo"></img>
+        <i class="fas fa-h1    ">TIMETABLE</i>
       </header>
+      <content className="App-content">
+        
+      </content>
+      <footer className="App-footer">
+
+      </footer>
     </div>
   );
 }
